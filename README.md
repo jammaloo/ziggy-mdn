1154605e6facdf8561fad43b7590358a3a3ea2bb
James Freeman <jfreeman@activecampaign.com>
2015-05-18 09:55:21 -0400

    Initial release with working !mdn command

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ziggy-mdn
-A Ziggy plugin to load js docs from the mozilla developer network
+A Ziggy plugin to load js docs from the mozilla developer network.
+
+It will search the given search term, and return a link and summary of the top result.
+
+Usage: !mdn <searchterm>
---
+++ index.js
@@ -0,0 +1,60 @@
+var request = require('request')
+
+module.exports = function(ziggy, settings) {
+	ziggy.on('pm', function (user, text) {
+		parseCommand(user, null, text)
+	})
+
+	ziggy.on('message', function (user, channel, text) {
+		parseCommand(user, channel, text)
+	})
+
+	function sendHelp(target) {
+		ziggy.say(target, 'Usage: !mdn <keyword>')
+	}
+
+	function parseCommand(user, channel, text) {
+		var target = channel || user.nick
+
+		//ignore messages that don't start with !mdn, only send help if we were pm'd
+		if(text.substr(0, 5) !== '!mdn ') {
+			if(text.substr(0,4) === '!mdn' || !channel) {
+				sendHelp(target)
+			}
+			return
+		}
+
+		var query = text.substr(5).trim()
+
+		if(!query) {
+			sendHelp(target)
+			return
+		}
+
+		getTopResult(query, function(result) {
+			if(!result.success) {
+				ziggy.say(target, result.error)
+				return
+			}
+
+			ziggy.say(target, result.result.title + ': ' + result.result.excerpt.replace(/<mark>|<\/mark>/g, '') + result.result.url)
+		})
+	}
+
+	function getTopResult(query, cb) {
+		request.get('https://developer.mozilla.org/en-US/search.json?topic=api&topic=html&topic=js&topic=webdev&q=' + encodeURIComponent(query), function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				if(!body) {
+					return cb({success: false, error: 'Error when loading MDN results'})
+				}
+
+				var results = JSON.parse(body)
+				if(!results.count) {
+					return cb({success: false, error: 'No MDN results found'})
+				}
+
+				return cb({success: true, result: results.documents[0]})
+			}
+		})
+	}
+}
